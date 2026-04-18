1fba23dc95a0e9cf4676abda8f0d2f239c4c0e98
Pratham Srivastava <prathamsrivastav123@gmail.com>
2026-04-18 16:32:32 +0530

    new live code

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
-//const MONGODB_URI = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;  //live ke leye uncomment kro isse 
+//const MONGODB_URI = "mongodb://127.0.0.1:27017/wanderlust";  //work krne ke leye localhost pr isse use kro 
 
 main()
   .then(() => {
@@ -34,8 +34,8 @@ main()
   });
 
 async function main() {
-  //await mongoose.connect(MONGODB_URI);  //old
-  await mongoose.connect(dbUrl); //s-new
+  //await mongoose.connect(MONGODB_URI);  //old-(work ke leye use kro)
+  await mongoose.connect(dbUrl); //s-new (use for live)
 }
 
 app.engine("ejs", ejsMate);
